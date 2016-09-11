100fb9bcd841e86fc1316260c1b0eac333297563
Max Hoffman <maximilian.wolfgang1@gmail.com>
2016-09-11 10:21:16 -0500

    Shoudl fix git directory issue

---
+++ Coign/LoginController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Coign
+//
+//  Created by Maximilian Hoffman on 9/3/16.
+//  Copyright © 2016 MaxMax. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    //MARK: - Stand-in for facebook login
+    @IBAction func loginButtonPressed(sender: UIButton) {
+        print("login pressed")
+        let storyboard = UIStoryboard(name: "MainMenu", bundle: nil)
+        let controller  = storyboard.instantiateInitialViewController()!
+        self.presentViewController(controller, animated: true, completion: nil)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
