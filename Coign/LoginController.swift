a3927edc9013a7b3068a4eb008676fa21ffe36c7
Max Hoffman <maximilian.wolfgang1@gmail.com>
2016-09-17 10:59:20 -0500

    Auto-updates to swift 3 syntax

--- Coign/LoginController.swift
+++ Coign/LoginController.swift
@@ -11,10 +11,10 @@ import UIKit
 class LoginController: UIViewController {
 
     //MARK: - Stand-in for facebook login
-    @IBAction func loginButtonPressed(sender: UIButton) {
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "MainApp", bundle: nil)
         let controller  = storyboard.instantiateInitialViewController()!
-        self.presentViewController(controller, animated: true, completion: nil)
+        self.present(controller, animated: true, completion: nil)
         
     }
     override func viewDidLoad() {
