2baa5865c68a09b51afefd5fa97345bec9fe5fe0
Max Hoffman <maximilian.wolfgang1@gmail.com>
2016-09-19 21:18:20 -0500

    Adds crisp logout. I still have to check if the tokens are cleared properly, but it looks nice

--- Coign/SideViewTableController.swift
+++ Coign/SideViewTableController.swift
@@ -9,31 +9,27 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class SideViewTableController: UITableViewController {
 
     @IBOutlet weak var logoutButton: UITableViewCell!
     
-    //logout, return to login screen
-    //func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-        
-        //logout
-        //try! FIRAuth.auth()?.signOut()
-        
-        //dismiss modal
-        //self.dismiss(animated: true, completion: nil)
-        
-        //segue to login screen
-        //let storyboard = UIStoryboard(name: "Login", bundle: nil)
-        //let controller  = storyboard.instantiateInitialViewController()!
-        //self.present(controller, animated: true, completion: nil)
-    //}
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if (indexPath.section == 1 && indexPath.row == 1){
-            //logout
+            
+            //give button permission to logout of facebook
+            let facebookLogin = FBSDKLoginManager()
+            
+            //logout of firebase/facebook
+            facebookLogin.logOut()
             try! FIRAuth.auth()!.signOut()
             print("should have logged out")
+            
+            //MARK: - is this necessary?
+            //FBSDKAccessToken.setCurrent(nil)
+            //FBSDKProfile.setCurrent(nil)
             
             //segue to login screen
             let storyboard = UIStoryboard(name: "Login", bundle: nil)
