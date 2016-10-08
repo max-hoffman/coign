eee8affd8a9e526facaf85c96e4f693dc5acdbae
Max Hoffman <maximilian.wolfgang1@gmail.com>
2016-10-08 12:06:36 -0500

    The new user setup is functional and looks nicer. I have not finished the data saving upon popover dismissal

--- Coign/MainMenuController.swift
+++ Coign/MainMenuController.swift
@@ -10,13 +10,78 @@ import UIKit
 
 class MainMenuController: UIViewController {
 
-    @IBOutlet weak var menuButton: UIBarButtonItem!
+
+
+    //properties
+    var blurView: UIVisualEffectView?
+    var blurEffect: UIVisualEffect?
+    
+    //UI outlets and actions
     @IBOutlet weak var userSetupPopover: UIView!
+    @IBAction func dismissPopover(_ sender: UIButton) {
+        dismissUserPopover()
+    }
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var charityPreferencePicker: UIPickerView!
+    
+    /**
+     Shows the user setup popover window. First time users add their phone number and email address to complete user sign-up. 
+     
+     Detailed description: creates a nil blur view, shows a popover window, and then animates the showing of the window and blur view, while hiding the navigation bar.
+    */
+    public func presentUserSetupPopover() {
+        
+        //create blur view with nil effect, add to view
+        blurView = UIVisualEffectView()
+        blurView?.frame = view.frame
+        self.view.addSubview(self.blurView!)
+
+        
+        //show popover
+        self.view.addSubview(userSetupPopover)
+        userSetupPopover.layer.cornerRadius = 10 //rounded corners
+        userSetupPopover.center = self.view.center
+
+        //make popover transparent and expanded (temporarily)
+        userSetupPopover.transform = CGAffineTransform.init(scaleX: 1.5, y: 1.5)
+        userSetupPopover.alpha = 0
+        
+        //animate default popover presentation
+        UIView.animate(withDuration: 0.5) {
+            
+            //apply blur
+            self.blurView?.effect = UIBlurEffect(style: .light)
+            
+            //restore popover defaults
+            self.userSetupPopover.alpha = 1
+            self.userSetupPopover.transform = CGAffineTransform.identity
+            
+            //hide navigation bar
+            self.navigationController?.setNavigationBarHidden(true, animated: true)
+        }
+    }
+    
+    /**
+     Remove popover view, save the data user has entered.
+    */
+     private func dismissUserPopover() {
+        userSetupPopover.removeFromSuperview()
+        blurView?.removeFromSuperview()
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    //MARK: - Superview and load functions
+    
+    //outlets
+    @IBOutlet weak var menuButton: UIBarButtonItem!
     
     override func viewDidLoad() {
-
         super.viewDidLoad()
         // Do view setup here.
+        
+        presentUserSetupPopover()
         
         //MARK: this should be standardized so we're not duplicating code
         if self.revealViewController() != nil {
@@ -24,7 +89,7 @@ class MainMenuController: UIViewController {
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
-    
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -32,22 +97,4 @@ class MainMenuController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    public func presentUserSetupPopover() {
-        print("attempted to present popover")
-        navigationController?.setNavigationBarHidden(true, animated: true)
-        //blur and vibrancy
-        //let blurAndVirbrancyView = UIVisualEffectView(effect: UIVibrancyEffect(blurEffect: UIBlurEffect(style: .dark)))
-        let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
-        blurView.frame = view.frame
-        view.addSubview(blurView)
-        
-        //show popover
-        self.view.addSubview(userSetupPopover)
-        userSetupPopover.layer.cornerRadius = 5
-        userSetupPopover.center = self.view.center
-    }
-    
-    private func dismissUserPopover() {
-        
-    }
 }
