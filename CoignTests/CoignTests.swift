80a163ab3d11295d51f0d52ef31416b89b124894
Max Hoffman <maximilian.wolfgang1@gmail.com>
2016-10-22 20:39:48 -0500

    Started unit tests, going to switch to a new branch

--- CoignTests/CoignTests.swift
+++ CoignTests/CoignTests.swift
@@ -20,17 +20,11 @@ class CoignTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testDateExtensions() {
+        let date = Date().shortDate
+        XCTAssertNotNil(date)
+        XCTAssertEqual(date.characters.count, 8)
     }
     
 }
