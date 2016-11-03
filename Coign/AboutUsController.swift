5b0a5f2169bd05a3c7aeb60a8dc9a31861cd261a
Max Hoffman <maximilian.wolfgang1@gmail.com>
2016-11-03 09:38:12 -0500

    Animations work with text now. Saving because there was so much trial and error to get to this point, still have UI tweaking to do.

--- Coign/AboutUsController.swift
+++ Coign/AboutUsController.swift
@@ -13,9 +13,33 @@ class AboutUsController: UIViewController {
     @IBOutlet weak var menuButton: UIBarButtonItem!
     
     @IBOutlet var aboutViews: [UIView]!
-    
     @IBOutlet var aboutButtons: [UIButton]!
 
+    //need body views and texts
+    @IBOutlet var textViews: [UIView]! {
+        didSet {
+            textViews.forEach {
+                $0.isHidden = true
+            }
+        }
+    }
+    
+    @IBOutlet var textLabels: [UILabel]! {
+        didSet {
+            textLabels.forEach {
+                $0.text = labelDictionary[$0.text!] ?? "error loading text"
+            }
+        }
+    }
+    
+    let labelDictionary = [
+        "mission text" : "At Coign, we strive to make donation and personal finance simple, social and substantial. If you would like to incorporate microdonation in your life by helping communities, thanking friends or claiming coupons, then Coign puts you in a position to take action by helping others.",
+        "features text" : "We are currently testing the Beta version of Coign, which allows users to donate individual dollars to a handful of prominent charities. We do encourage users to share their donation messages on social media as a way to encourage others to take action in their own small way. We do not support donations larger than $1.50.",
+        "team text": "The team at Coign is currently two recent post-graduates. We hope to grow Coign to a point where we can work full-time on trying to integrate microdonation into society in a way that benefits donators, as well as local and national communities.",
+        "opportunities text": "Ambitious and talented students who have heavy technical backgrounds, lack risk-aversion, and who are interested in coufounding a startup are welcome to contact our team for more information.",
+        
+        ]
+    
     @IBAction func viewSelected(sender: UIButton) {
         if let index = aboutButtons.index(of: sender) {
             
@@ -24,22 +48,18 @@ class AboutUsController: UIViewController {
                 if self.aboutViews[0].isHidden || self.aboutViews[1].isHidden {
                     self.aboutViews.forEach {
                         $0.isHidden = false
-//                        ($0.viewWithTag(1) as! UILabel).text = ""
                     }
-                    self.aboutButtons.forEach {
-                        $0.isHidden = false
-                    }
+                    self.textViews[index].isHidden = true
+
                 }
                 else {
                     self.aboutViews.forEach {
                         $0.isHidden = true
                     }
-                    self.aboutButtons.forEach {
-                        $0.isHidden = true
-                    }
+                    self.textViews[index].isHidden = false
                     self.aboutViews[index].isHidden = false
-                    self.aboutButtons[index].isHidden = false
-//                    (self.aboutViews[index].viewWithTag(1) as! UILabel).text = "practice"
+                   
+
                 }
 
             })
