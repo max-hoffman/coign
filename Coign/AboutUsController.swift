2200553f8d58f5260acc6ba9fc9dc9cf9392bf09
Max Hoffman <maximilian.wolfgang1@gmail.com>
2016-11-03 16:50:25 -0500

    Tried to improve the UI and fonts as much as I could

--- Coign/AboutUsController.swift
+++ Coign/AboutUsController.swift
@@ -34,10 +34,9 @@ class AboutUsController: UIViewController {
     
     let labelDictionary = [
         "mission text" : "At Coign, we strive to make donation and personal finance simple, social and substantial. If you would like to incorporate microdonation in your life by helping communities, thanking friends or claiming coupons, then Coign puts you in a position to take action by helping others.",
-        "features text" : "We are currently testing the Beta version of Coign, which allows users to donate individual dollars to a handful of prominent charities. We do encourage users to share their donation messages on social media as a way to encourage others to take action in their own small way. We do not support donations larger than $1.50.",
-        "team text": "The team at Coign is currently two recent post-graduates. We hope to grow Coign to a point where we can work full-time on trying to integrate microdonation into society in a way that benefits donators, as well as local and national communities.",
+        "features text" : "We are currently testing the Beta version of Coign, which allows users to donate individual dollars to a handful of prominent charities. We do encourage users to share their donation messages on social media as a way to encourage others to take action in their own ways. We do not support donations larger than $1.50.",
+        "team text": "The team at Coign includes two recent college graduates. We hope to grow Coign to a point where we can work full-time trying to integrate microdonation into society in a way that benefits both donators and their communities. We do have day jobs currently, and we'd like to enourage users who have advice and/or criticism to contact us and help improve the app.",
         "opportunities text": "Ambitious and talented students with heavy technical backgrounds, who lack risk-aversion, and who are interested in cofounding a startup are welcome to contact our team for more information.",
-        
         ]
     
     @IBAction func viewSelected(sender: UIButton) {
